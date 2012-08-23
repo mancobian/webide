1900fceccf5a8b0233317bc229d251d37a0eedd6
Mancobian Poemandres <mancobian.poemandres@gmail.com>
2012-08-22 20:45:03 -0400

    Add support for simple console commands.

--- scripts/app/index.js
+++ scripts/app/index.js
@@ -6,37 +6,41 @@ var initialize = function() {
   Console.initialize(Module);
 };
 
+var ApplicationController = function($scope) {
+  $scope.editor = { url: 'scripts/editor/layout.partial' };
+  $scope.console = { url: 'scripts/console/layout.partial', visible: false, el: $("#console") };
+  $scope.debug = 0;
+
+  $scope.$on('$includeContentLoaded', function(event) {
+    Editor.create();
+    $scope.console.el.hide();
+  });
+
+  $scope.onKeyDown = function(key) {
+    $scope.debug = "Keycode: " + key;
+
+    switch (key) {
+      case 112: { // F1
+        $scope.debug = "Key: F1";
+        $scope.toggleConsole();
+        break;
+      }
+      default: {
+        break;
+      }
+    }; // end switch()
+  };
+
+  $scope.toggleConsole = function() {
+    $scope.console.visible = !$scope.console.visible;
+    $scope.debug = "console.visible = " + $scope.console.visible
+    $scope.console.visible ? $scope.console.el.show() : $scope.console.el.hide();
+  };
+};
+
 var Module = angular.module('webide', []).
-  controller('ApplicationController', [ '$scope', function ApplicationController($scope) {
-    $scope.editor = { url: 'scripts/editor/layout.partial' };
-    $scope.console = { url: 'scripts/console/layout.partial', visible: false, el: $("#console") };
-    $scope.debug = 0;
-
-    $scope.$on('$includeContentLoaded', function(event) {
-      Editor.create();
-      $scope.console.el.hide();
-    });
-
-    $scope.onKeyDown = function(key) {
-      $scope.debug = "Keycode: " + key;
-
-      switch (key) {
-        case 112: { // F1
-          $scope.debug = "Key: F1";
-          $scope.toggleConsole();
-          break;
-        }
-        default: {
-          break;
-        }
-      }; // end switch()
-    };
-
-    $scope.toggleConsole = function() {
-      $scope.console.visible = !$scope.console.visible;
-      $scope.debug = "console.visible = " + $scope.console.visible
-      $scope.console.visible ? $scope.console.el.show() : $scope.console.el.hide();
-    };
+  controller('ApplicationController', [ '$scope', function($scope) {
+    ApplicationController($scope);
   }]).
   directive('onKeyDown', function() {
     return function(scope, element, attributes) {
--- scripts/console/index.js
+++ scripts/console/index.js
@@ -3,22 +3,76 @@ function( $, _, Angular ) {
 
 var Module = null;
 
+var ConsoleController = function($scope) {
+  $scope.console = {
+    text: "",
+  };
+
+  $scope.addConsoleHistoryItem = function(command, item, classType) {
+    var tag = "div";
+    if (classType === "response") {
+      tag = "pre"; 
+    }
+
+    // Add the new item to the console history
+    var html = "<div class='command console-history-item'>" + command + "</div>";
+    html += "<" + tag + " class='" + classType + " console-history-item'>" + item + "</" + tag + ">";
+    $('#console-history').append(html);
+
+    // Forces console history to scroll to the bottom without delay
+    $('#console-history').animate(
+      { scrollTop: $('#console-history')[0].scrollHeight }, 0
+    );
+  }; // end function addConsoleHistoryItem()
+
+  $scope.executeCommand = function() {
+    var command = $scope.console.text;
+
+    switch (command) {
+      case "clear": {
+        $('#console-history').html('');
+        break;
+      }
+      case "open": {
+        var filename = tokens[1];
+        now.openFile (filename, function(error, content) {
+          var mode = Mode.getModeFromPath(filename);
+          console.editor.getSession().setValue(content);
+          console.editor.getSession().modeName = mode.name;
+          console.editor.getSession().setMode(mode.mode);
+          $scope.addConsoleHistoryItem(command, 'Ok.', 'response');
+        });
+        break;
+      }
+      case "ls": {
+        now.exec (command, function(result) {
+          if (result) {
+            $scope.addConsoleHistoryItem(command, result, 'response');
+          }
+        });
+        break;
+      }
+      default: {
+        // @todo ...
+        // addConsoleHistoryItem("Unsupported command: " + command, 'error');
+        now.exec (command, function(result) {
+          if (result) {
+            $scope.addConsoleHistoryItem(command, result, 'response');
+          }
+        });
+        break;
+      }
+    }; // end switch() 
+
+    $scope.console.text = "";
+  }; // end function executeCommand() 
+};
+
 var initialize = function(App) {
-
   Module = App.
-    controller('ConsoleController', [ '$scope', function ConsoleController($scope) {
-      $scope.console = {
-        text: "",
-      };
-
-      $scope.executeCommand = function() {
-        // alert($scope.console.text);
-        $scope.console.text = "";
-      }; 
-
-    }] // end function()
-  ); // end controller()
-
+    controller('ConsoleController', [ '$scope', function($scope) {
+      ConsoleController($scope);
+    }]);
 };
 
 return {
